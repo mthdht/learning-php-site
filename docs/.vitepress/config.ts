a569f8586a394304badb3396c23bea48bd447641
null <null>
2025-09-30 09:07:39 +0200

    change base path

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   lang: 'fr-FR',
   title: 'Apprendre PHP',
   description: 'Vite & Vue powered static site generator.',
-  base: '',
+  base: 'learning-php-site',
 
   themeConfig: {
     nav: [
