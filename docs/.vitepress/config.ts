63507f424d0569d3953924d9d8f8bdd443d7f715
null <null>
2025-09-30 09:54:06 +0200

    made sidebar links for module 1

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -9,7 +9,14 @@ export default defineConfig({
 
   themeConfig: {
     nav: [
-      { text: 'Example', link: '/example' },
+      { 
+        text: 'Modules PHP', 
+        items: [
+          { text: 'Module 1: PHP fondamentaux', link: '/Module-1/introduction' },
+          { text: 'Module 2: PHP Web et formulaires', link: '/Module-2/introduction' },
+          { text: 'Module 3: PHP fondamentaux', link: '/Module-1/introduction' },
+        ]
+      },
 
       // {
       //   text: 'Dropdown Menu',
@@ -23,15 +30,90 @@ export default defineConfig({
       // ...
     ],
 
-    sidebar: [
-      {
-        // text: 'Guide',
-        items: [
-          { text: 'Example', link: '/example' },
-          // ...
-        ],
-      },
-    ],
+    sidebar: {
+      '/module-1/': [
+        {
+          text: 'Programme',
+          link: '/programme'
+        },
+        {
+          text: 'Module 1: PHP fondamentaux',
+          collapsed: false,
+          items: [
+            {
+              text: 'Introduction.',
+              link: '/module-1/introduction'
+            },
+            {
+              text: 'Premiers pas avec php.',
+              link: '/module-1/chapitres/chapitre-1-premiers-pas-avec-php'
+            },
+            {
+              text: 'Les variables et types de données.',
+              link: '/module-1/chapitres/chapitre-2-les-variables-et-types-de-donnees'
+            },
+            {
+              text: 'Opérateurs et expressions.',
+              link: '/module-1/chapitres/chapitre-3-operateurs-et-expressions'
+            },
+            {
+              text: 'Structures de contrôle.',
+              link: '/module-1/chapitres/chapitre-4-structures-de-controle'
+            },
+            {
+              text: 'Les tableaux.',
+              link: '/module-1/chapitres/chapitre-5-les-tableaux'
+            },
+            {
+              text: 'Les fonctions.',
+              link: '/module-1/chapitres/chapitre-6-les-fonctions'
+            },
+            {
+              text: 'Chaines de caractères.',
+              link: '/module-1/chapitres/chapitre-7-chaines-de-caracteres'
+            },
+            {
+              text: 'exercices',
+              collapsed: true,
+              items: [
+                {
+                  text: 'Premiers pas avec php.',
+                  link: '/module-1/exercices/chapitre-1-premiers-pas-avec-php'
+                },
+                {
+                  text: 'Les variables et types de données.',
+                  link: '/module-1/exercices/chapitre-2-les-variables-et-types-de-donnees'
+                },
+                {
+                  text: 'Opérateurs et expressions.',
+                  link: '/module-1/exercices/chapitre-3-operateurs-et-expressions'
+                },
+                {
+                  text: 'Structures de contrôle.',
+                  link: '/module-1/exercices/chapitre-4-structures-de-controle'
+                },
+                {
+                  text: 'Les tableaux.',
+                  link: '/module-1/exercices/chapitre-5-les-tableaux'
+                },
+                {
+                  text: 'Les fonctions.',
+                  link: '/module-1/exercices/chapitre-6-les-fonctions'
+                },
+                {
+                  text: 'Chaines de caractères.',
+                  link: '/module-1/exercices/chapitre-7-chaines-de-caracteres'
+                },
+              ]
+            },
+            {
+              text: 'Conclusion.',
+              link: '/module-1/conclusion'
+            },
+          ],
+        },
+      ]
+    },
     docFooter: {
       prev: 'Page précédente',
       next: 'Page suivante'
